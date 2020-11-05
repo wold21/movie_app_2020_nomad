9f3703d5cfb9529ebad38ced45c4255df70cc4f5
wold21 <wold2180@gmail.com>
2020-11-05 17:48:15 +0900

    day 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import PropTypes from 'prop-types';
+
+
+//JSX -> HTML + JavaScript
+// npm i prop-types -> 인자(props)를 잘 넘겨주고 받는지 체크해줌.
+
+const foodILike = [
+  {
+    id:1,
+    name : "kimchi",
+   color : "red",
+   rating: 5
+  },
+  {
+    id:2,
+    name : "Ramen",
+   color : "yellow",
+   rating: 3
+  },
+  {
+    id:3,
+    name : "Pizza",
+   color : "red & beige",
+   rating: 3.4
+  },
+  {
+    id:4,
+    name : "Hamburger",
+   color : "green, red, brown",
+   rating: 5
+}];
+
+// 만약 /5.0에서 에러가 난다면 터미널에서 npm i 해주기.
+function Food({name, color, rating}){
+  return <div>
+      <h2>I Like {name}</h2>
+      <h2>{color}</h2>
+      <h4>{rating}/5.0</h4> 
+  </div>
+};
+
+//propType의 이름은 propType으로 고정해서 사용해야함.
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  color: PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {foodILike.map(dish => (
+        <Food 
+        key={dish.id} 
+        name={dish.name} 
+        color={dish.color} 
+        rating={dish.rating} 
+        />
+        ))}
     </div>
   );
 }
